777570b3e9a1ed9c535b1b55a9a9dadc43709168
lihawhaw <lihawhaw@users.noreply.github.com>
2022-07-13 12:36:43 +0000

    deploy: c8b15b996e98ee321760aba84a2c219b8f5d43d2

--- assets/about.8c54a5cf.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./home.1d95160e.js";import{o as r,c as e}from"./vue.e9ede0e9.js";const c={};function t(n,a){return r(),e("div",null,"about")}var f=o(c,[["render",t]]);export{f as A};
